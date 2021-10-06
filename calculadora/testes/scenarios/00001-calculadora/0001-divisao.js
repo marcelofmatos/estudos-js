e6c2ecaaff334b33046ea529906165a70e3dd75e
Marcelo Matos <marcelofmatos@gmail.com>
2021-10-06 18:32:55 -0300

    divisão e raiz quadrada

---
+++ calculadora/testes/scenarios/00001-calculadora/0001-divisao.js
@@ -0,0 +1,13 @@
+
+'use strict';
+
+module.exports = (page, cb) => {
+	(async () => {
+		await page.goto(`https://marcelomatos.dev/estudos-www/calculadora/`, {waitUntil: 'networkidle0'});
+		await page.type('#CampoA', '144');
+		await page.type('#CampoB', '12');
+		await page.click('.caixa button:nth-child(4)');
+		await page.$eval('#Resultado', (el, value) => el.value = value, '12');
+		cb();
+	})();
+};
--- calculadora/testes/scenarios/00001-calculadora/0002-raiz-quadrada.js
+++ calculadora/testes/scenarios/00001-calculadora/0002-raiz-quadrada.js
@@ -4,8 +4,9 @@
 module.exports = (page, cb) => {
 	(async () => {
 		await page.goto(`https://marcelomatos.dev/estudos-www/calculadora/`, {waitUntil: 'networkidle0'});
-		await page.type('#CampoA', 144);
-		await page.click('.caixa button:nth-child(5)')
+		await page.type('#CampoA', '144');
+		await page.click('.caixa button:nth-child(5)');
+		await page.$eval('#Resultado', (el, value) => el.value = value, '12');
 		cb();
 	})();
 };
